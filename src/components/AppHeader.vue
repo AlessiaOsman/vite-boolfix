26b749021959aaff42047ec9787da85125be0908
AlessiaOsman <alessia.osman@gmail.com>
2024-02-08 09:04:25 +0100

    created header and search bar

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+    import BaseSearch from './search/BaseSearch.vue';
+  export default {
+    name: 'AppHeader',
+    components: {BaseSearch}
+  }
+</script>
+
+<template>
+  <header>
+    <h1>Boolfix</h1>
+    <BaseSearch @submit-search=""/>
+  </header>
+</template>
+
---
+++ src/components/search/BaseSearch.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'BaseSearch',
+    data: () => ({
+        searchText: ''
+    }),
+    emits: ['submit-search']
+}
+</script>
+
+<template>
+    <div>
+        <form @submit.prevent="$emit('submit-search')">
+            <input type="text" v-model="searchText">
+        </form>
+    </div>
+</template>
