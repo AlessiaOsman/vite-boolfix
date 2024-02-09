b8d22c738f401691dfa859c1cded96667ac56c22
AlessiaOsman <alessia.osman@gmail.com>
2024-02-09 17:01:09 +0100

    header style

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,9 +9,24 @@
 </script>
 
 <template>
-  <header>
+  <header class="d-flex justify-content-between align-items-center p-4">
     <h1>Boolfix</h1>
     <BaseSearch @text-change="$emit('set-text', $event)"  @submit-search="$emit('search-data')"/>
   </header>
 </template>
+
+<style lang="scss" scoped>
+  header{
+    position: sticky;
+    top: 0;
+    right: 0;
+    left: 0;
+    background-color: black;
+
+    h1{
+      color: red;
+    }
+    
+  }
+</style>
 
--- src/components/search/BaseSearch.vue
+++ src/components/search/BaseSearch.vue
@@ -11,9 +11,13 @@ export default {
 
 <template>
     <div>
-        <form @submit.prevent="$emit('submit-search')">
-            <input @keyup="$emit('text-change', searchText)" type="text" v-model.trim="searchText">
-            <button>cerca</button>
+        <form class="row g-3" @submit.prevent="$emit('submit-search')">
+            <div class="col-auto">
+            <input class="form-control" @keyup="$emit('text-change', searchText)" type="text" v-model.trim="searchText">
+        </div>
+        <div class="col-auto">
+            <button class="btn btn-danger mb-3">cerca</button></div>
+
         </form>
     </div>
 </template>
